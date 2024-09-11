eeb98df30789393351864ee82d525a323110575e
juan12331 <juanxadez2@gmail.com>
2024-09-11 15:18:07 -0300

    arrumando problema do overfload (sla como escreve kkkkk)

--- lib/pages/cadastro.dart
+++ lib/pages/cadastro.dart
@@ -42,131 +42,135 @@ class Cadastro extends StatelessWidget {
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(4.5),
                 ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    // Title text at the top of the form
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 16.0),
-                      child: Text(
-                        'Cadastra-se',
-                        style: TextStyle(
-                          fontSize: 32,
-                          fontWeight: FontWeight.bold,
-                          color: const Color.fromARGB(255, 0, 0, 0),
-                        ),
-                      ),
-                    ),
-                    // Form fields
-                    TextFormField(
-                      autofocus: true,
-                      decoration: InputDecoration(
-                        icon: Icon(Icons.people_rounded),
-                        hintText: "Nome",
-                      ),
-                    ),
-                    SizedBox(height: 16),
-                    TextFormField(
-                      autofocus: true,
-                      decoration: InputDecoration(
-                        icon: Icon(Icons.edit_document),
-                        hintText: "CPF",
-                      ),
-                    ),
-                    SizedBox(height: 16),
-                    TextFormField(
-                      autofocus: true,
-                      decoration: InputDecoration(
-                        icon: Icon(Icons.phone_android),
-                        hintText: "celular",
-                      ),
-                    ),
-                    SizedBox(height: 16),
-                    TextFormField(
-                      autofocus: true,
-                      decoration: InputDecoration(
-                        icon: Icon(Icons.mail),
-                        hintText: "Email",
-                      ),
-                    ),
-                    SizedBox(height: 16),
-
-                    TextFormField(
-                      autofocus: true,
-                      decoration: InputDecoration(
-                        icon: Icon(Icons.lock),
-                        hintText: "Senha",
-                        suffixIcon: GestureDetector(
-                          child: Icon(
-                            Icons.visibility,
+                child: SingleChildScrollView(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        // Title text at the top of the form
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 16.0),
+                          child: Text(
+                            'Cadastra-se',
+                            style: TextStyle(
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                              color: const Color.fromARGB(255, 0, 0, 0),
+                            ),
                           ),
                         ),
-                      ),
-                      obscureText: true,
-                    ),
-                    SizedBox(height: 20),
-                    TextFormField(
-                      autofocus: true,
-                      decoration: InputDecoration(
-                        icon: Icon(Icons.lock),
-                        hintText: "Confirmar Senha",
-                        suffixIcon: GestureDetector(
-                          child: Icon(
-                            Icons.visibility,
+                        // Form fields
+                        TextFormField(
+                          autofocus: true,
+                          decoration: InputDecoration(
+                            icon: Icon(Icons.people_rounded),
+                            hintText: "Nome",
                           ),
                         ),
-                      ),
-                      obscureText: true,
+                        SizedBox(height: 16),
+                        TextFormField(
+                          autofocus: true,
+                          decoration: InputDecoration(
+                            icon: Icon(Icons.edit_document),
+                            hintText: "CPF",
+                          ),
+                        ),
+                        SizedBox(height: 16),
+                        TextFormField(
+                          autofocus: true,
+                          decoration: InputDecoration(
+                            icon: Icon(Icons.phone_android),
+                            hintText: "celular",
+                          ),
+                        ),
+                        SizedBox(height: 16),
+                        TextFormField(
+                          autofocus: true,
+                          decoration: InputDecoration(
+                            icon: Icon(Icons.mail),
+                            hintText: "Email",
+                          ),
+                        ),
+                        SizedBox(height: 16),
+                    
+                        TextFormField(
+                          autofocus: true,
+                          decoration: InputDecoration(
+                            icon: Icon(Icons.lock),
+                            hintText: "Senha",
+                            suffixIcon: GestureDetector(
+                              child: Icon(
+                                Icons.visibility,
+                              ),
+                            ),
+                          ),
+                          obscureText: true,
+                        ),
+                        SizedBox(height: 20),
+                        TextFormField(
+                          autofocus: true,
+                          decoration: InputDecoration(
+                            icon: Icon(Icons.lock),
+                            hintText: "Confirmar Senha",
+                            suffixIcon: GestureDetector(
+                              child: Icon(
+                                Icons.visibility,
+                              ),
+                            ),
+                          ),
+                          obscureText: true,
+                        ),
+                        // Button and other text elements
+                        SizedBox(height: 20),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => Login()),
+                            );
+                          },
+                          child: const Text(
+                            "Entrar",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+                            fixedSize: Size(100, 35),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12.5),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Divider(),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => Login()),
+                            );
+                          },
+                          child: const Text(
+                            "Ja tenho conta",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color.fromARGB(255, 5, 160, 0),
+                            fixedSize: Size(150, 35),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12.5),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                      ],
                     ),
-                    // Button and other text elements
-                    SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => Login()),
-                        );
-                      },
-                      child: const Text(
-                        "Entrar",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color.fromARGB(255, 0, 0, 0),
-                        fixedSize: Size(100, 35),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12.5),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    Divider(),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => Login()),
-                        );
-                      },
-                      child: const Text(
-                        "Ja tenho conta",
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color.fromARGB(255, 5, 160, 0),
-                        fixedSize: Size(150, 35),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12.5),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                  ],
+                  ),
                 ),
               ),
             ),
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -8,29 +8,26 @@ class Login extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 54, 69, 79),
-      
       appBar: AppBar(
-        
         backgroundColor: const Color.fromARGB(255, 27, 18, 18),
-
         flexibleSpace: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Bem-vindo',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Bem-vindo',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
-
-      
       body: Stack(
         children: [
           // Container for the form
@@ -47,97 +44,110 @@ class Login extends StatelessWidget {
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(4.5),
                 ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    // Title text at the top of the form
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 16.0),
-                      child: Text(
-                        'Login',
-                        style: TextStyle(
-                          fontSize: 32,
-                          fontWeight: FontWeight.bold,
-                          color: const Color.fromARGB(255, 0, 0, 0),
-                        ),
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      // Title text at the top of the form
+                  
+                      Image(
+                        image: AssetImage(
+                            'Perfil.png'), // ou NetworkImage para imagens da web
+                        width: 100, // Largura da imagem
+                        height: 100, // Altura da imagem
+                        fit: BoxFit
+                            .cover, // Como a imagem deve se ajustar ao tamanho
                       ),
-                    ),
-                    // Form fields
-                    TextFormField(
-                      autofocus: true,
-                      decoration: InputDecoration(
-                        icon: Icon(Icons.mail),
-                        hintText: "Informe o email",
-                      ),
-                    ),
-                    SizedBox(height: 16),
-                    TextFormField(
-                      autofocus: true,
-                      decoration: InputDecoration(
-                        icon: Icon(Icons.lock),
-                        hintText: "Informe a senha",
-                        suffixIcon: GestureDetector(
-                          child: Icon(
-                            Icons.visibility_off,
+                  
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 16.0),
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                            fontSize: 32,
+                            fontWeight: FontWeight.bold,
+                            color: const Color.fromARGB(255, 0, 0, 0),
                           ),
                         ),
                       ),
-                      obscureText: true,
-                    ),
-                    SizedBox(height: 20),
-                    // Button and other text elements
-                    ElevatedButton(
-  onPressed: () {},
-  child: const Text(
-    "Entrar",
-    style: TextStyle(
-      color: Colors.white,
-      fontWeight: FontWeight.bold,
-    ),
-  ),
-  style: ElevatedButton.styleFrom(
-    backgroundColor: const Color.fromARGB(255, 0, 0, 0),
-    fixedSize: Size(100, 35),
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(12.5),
-    ),
-  ),
-),
-                    SizedBox(height: 10),
-                    Divider(),
-                    Text(
-                      "Esqueci a senha",
-                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                    ),
-                    SizedBox(height: 10),
-                    ElevatedButton(
-  onPressed: () {
-     Navigator.push(
-    context,
-    MaterialPageRoute(builder: (context) => Cadastro()),
-  );
-  },
-  child: const Text(
-    "Cadastre-se",
-    style: TextStyle(
-      color: Colors.white,
-    ),
-  ),
-  style: ElevatedButton.styleFrom(
-    backgroundColor: const Color.fromARGB(255, 5, 160, 0),
-    fixedSize: Size(125, 35),
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(12.5),
-    ),
-  ),
-),
-                  ],
+                  
+                      // Form fields
+                      TextFormField(
+                        autofocus: true,
+                        decoration: InputDecoration(
+                          icon: Icon(Icons.mail),
+                          hintText: "Informe o email",
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      TextFormField(
+                        autofocus: true,
+                        decoration: InputDecoration(
+                          icon: Icon(Icons.lock),
+                          hintText: "Informe a senha",
+                          suffixIcon: GestureDetector(
+                            child: Icon(
+                              Icons.visibility_off,
+                            ),
+                          ),
+                        ),
+                        obscureText: true,
+                      ),
+                      SizedBox(height: 20),
+                      // Button and other text elements
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: const Text(
+                          "Entrar",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+                          fixedSize: Size(100, 35),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12.5),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Divider(),
+                      Text(
+                        "Esqueci a senha",
+                        style:
+                            TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                      ),
+                      SizedBox(height: 10),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => Cadastro()),
+                          );
+                        },
+                        child: const Text(
+                          "Cadastre-se",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(255, 5, 160, 0),
+                          fixedSize: Size(125, 35),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12.5),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
           ),
           // Positioned Text above the form
-          
         ],
       ),
     );
