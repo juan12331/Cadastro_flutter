f35c081087f66b8c51be0aa873937d12a1deacab
juan12331 <juanxadez2@gmail.com>
2024-09-24 14:32:36 -0300

    colocando tema padrão

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:terceiro/shared/style.dart';
 import 'pages/login.dart';
 import 'pages/cadastro.dart';
 import 'pages/home.dart';
@@ -13,6 +14,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      theme: ThemeData(
+        fontFamily: MyFonts.fontPrime,
+        colorScheme: ColorScheme.fromSeed(seedColor: MyColors.cinza),
+        useMaterial3: true,
+      ),
       routes: {
         '/': (context) => const Login(),
         '/cadastro': (context) => const Cadastro(),
